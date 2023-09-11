8bee314ea4220596833d11882a750dd6d2c04496
jerzka <kjerzmanowska@gmail.com>
2023-09-10 22:25:20 -0500

    Redesign hero section of the index page
    - Adding a responsive hero section in the hero handlebar template;
    - Changing website fonts;
    - Adding dynamic change of navigation bar icon from the hamburger icon to close icon in index.js and differentiating sticky menu up-arrow button from navigation bar ;
    - Renaming button name from "Register" to "Sign up";

--- client/public/index.js
+++ client/public/index.js
@@ -1,9 +1,24 @@
 const logoHomeBtn = document.getElementById("logo");
-logoHomeBtn.addEventListener("click", () => {
-    window.location = "/home";
-});
+// logoHomeBtn.addEventListener("click", () => {
+//     window.location = "/home";
+// });
 
 const signinBtn = document.getElementById("signinBtn");
 signinBtn.addEventListener("click", () => {
     window.location = "/signin";
+});
+
+
+const toggleBtn = document.getElementsByClassName("navbar-toggler")[0];
+toggleBtn.addEventListener("click", () =>{
+let isCollapsed = toggleBtn.classList.contains("collapsed");
+if(isCollapsed){
+    toggleBtn.firstElementChild.classList.remove('navbar-toggler-icon');
+    toggleBtn.firstElementChild.classList.add('navbar-toggler-icon-collapsed');
+}
+else{
+    toggleBtn.firstElementChild.classList.remove('navbar-toggler-icon-collapsed');
+    toggleBtn.firstElementChild.classList.add('navbar-toggler-icon');
+
+}
 });
